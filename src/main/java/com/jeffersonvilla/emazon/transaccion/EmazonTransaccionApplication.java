e403a1a82acf2b3dc231a46b47090862787a1664
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-09-17 13:55:15 -0500

    primer commit

---
+++ src/main/java/com/jeffersonvilla/emazon/transaccion/EmazonTransaccionApplication.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.emazon.transaccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonTransaccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonTransaccionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffersonvilla/emazon/transaccion/EmazonTransaccionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonvilla.emazon.transaccion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EmazonTransaccionApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertNull(null);
+	}
+
+}
